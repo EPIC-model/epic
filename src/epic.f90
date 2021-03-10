84a7d64c6a9e2747b67c5459505d99d3eef13e9e
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-10 11:28:30 +0100

    fix location of 'use' statement

--- src/epic.f90
+++ src/epic.f90
@@ -1,10 +1,9 @@
 program epic
     use model, only : pre_run, run, post_run
-    use parameters, only : filename, verbose
     implicit none
 
     ! Read command line (verbose, filename, etc.)
-    call parse_command_line(filename, verbose)
+    call parse_command_line
 
     ! Create the model
     call pre_run
@@ -19,12 +18,12 @@ end program epic
 
 
 ! Get the file name provided via the command line
-subroutine parse_command_line(filename, verbose)
+subroutine parse_command_line
+    use parameters, only : filename, verbose
     integer                          :: i
     character(len=32)                :: arg
-    character(len=32), intent(inout) :: filename
-    logical,           intent(inout) :: verbose
 
+    filename = ''
     i = 0
     do
         call get_command_argument(i, arg)
@@ -43,8 +42,8 @@ subroutine parse_command_line(filename, verbose)
     end do
 
     if (filename == '') then
-        write(*,*) 'No configuration file provided. Run code with "./epic --config [config file]"'
-        return
+        print *, 'No configuration file provided. Run code with "./epic --config [config file]"'
+        stop
     endif
 
     ! This is the main application of EPIC
