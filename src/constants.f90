88ed31a1fabb9975546413340ee0bd89a09153c2
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-19 14:38:43 +0100

    increase maximum parcel number

--- src/constants.f90
+++ src/constants.f90
@@ -4,6 +4,6 @@ module constants
     double precision, parameter :: pi = acos(-1.0d0)
 
     ! maximum number of allowed parcels
-    integer, parameter :: max_num_parcels = 1e4
+    integer, parameter :: max_num_parcels = 1e6
 
 end module
