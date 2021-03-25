692aa3bc9d3dd77cdcb3def07906b3f438710717
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-25 11:02:11 +0100

    add file parcel_merge.f90

---
+++ src/parcels/parcel_merge.f90
@@ -0,0 +1,13 @@
+module parcel_merge
+    use parcel_container, only : parcel_container_type
+    implicit none
+
+    contains
+        subroutine merge_ellipses(parcels)
+            type(parcel_container_type), intent(inout) :: parcels
+
+
+        end subroutine merge_ellipses
+
+
+end module parcel_merge
