9e67fa46018f3b5e112ea663d3daa0f186c423e8
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-29 10:00:51 +0200

    fix indentation

--- src/parcels/parcel_merge.f90
+++ src/parcels/parcel_merge.f90
@@ -107,9 +107,9 @@ module parcel_merge
 
 
             ! update center of mass
-            parcels%position(j, 1) =  mu1 * parcels%position(i, 1) &
+            parcels%position(j, 1) = mu1 * parcels%position(i, 1) &
                                    + mu2 * parcels%position(j, 1)
-            parcels%position(j, 2) =  mu1 * parcels%position(i, 2) &
+            parcels%position(j, 2) = mu1 * parcels%position(i, 2) &
                                    + mu2 * parcels%position(j, 2)
 
             ! update volume
