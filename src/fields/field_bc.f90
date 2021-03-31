92cee1ef62faa8e6b022ca702f70d025bd44a7fc
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-31 14:21:15 +0200

    fields: vertical bc in y; periodic bc in x

--- src/fields/field_bc.f90
+++ src/fields/field_bc.f90
@@ -2,9 +2,6 @@ module field_bc
     use parameters, only : mesh
     implicit none
 
-    private :: apply_free_slip,    &
-               apply_periodic
-
     contains
 
         subroutine apply_field_bc(field)
@@ -20,52 +17,29 @@ module field_bc
             field(:, mesh%grid(2), :) = field(:, mesh%grid(2), :)      &
                                       + field(:, mesh%grid(2) + 1, :)
 
-            do i = 1, 2
-                if (mesh%bc(i) == "free slip") then
-                    call apply_free_slip(field, i)
-                else if (mesh%bc(i) == "periodic") then
-                    call apply_periodic(field, i)
-                else
-                    print *, "No boundary condition named '", mesh%bc(i), "'."
-                    stop
-                endif
-            enddo
+            ! vertical bc
+            call apply_free_slip(field)
+
+            ! horizontal bc
+            call apply_periodic(field)
+
         end subroutine apply_field_bc
 
 
-        subroutine apply_periodic(field, i)
+        subroutine apply_periodic(field)
             double precision, intent(inout) :: field(0:, 0:, :)
-            integer,          intent(in)    :: i
 
-            if (i == 1) then
-                field(1, :, :)            = field(1, :, :)              &
-                                          + field(mesh%grid(1), :, :)
-                field(mesh%grid(1), :, :) = field(1, :, :)
-            else if (i == 2) then
-                field(:, 1, :)            = field(:, 1, :)              &
-                                          + field(:, mesh%grid(2), :)
-                field(:, mesh%grid(2), :) = field(:, 1, :)
-            else
-                print *, "Only up to 2 dimensions!"
-            endif
-
+            field(1, :, :)            = field(1, :, :)              &
+                                      + field(mesh%grid(1), :, :)
+            field(mesh%grid(1), :, :) = field(1, :, :)
         end subroutine apply_periodic
 
 
-        subroutine apply_free_slip(field, i)
+        subroutine apply_free_slip(field)
             double precision, intent(inout) :: field(0:, 0:, :)
-            integer,          intent(in)    :: i
 
-            if (i == 1) then
-                field(1, :, :)            = 2.0 * field(1, :, :)
-                field(mesh%grid(1), :, :) = 2.0 * field(mesh%grid(1), :, :)
-            else if (i == 2) then
-                field(:, 1, :)            = 2.0 * field(:, 1, :)
-                field(:, mesh%grid(2), :) = 2.0 * field(:, mesh%grid(2), :)
-            else
-                print *, "Only up to 2 dimensions!"
-            endif
-
+            field(:, 1, :)            = 2.0 * field(:, 1, :)
+            field(:, mesh%grid(2), :) = 2.0 * field(:, mesh%grid(2), :)
         end subroutine apply_free_slip
 
 end module field_bc
