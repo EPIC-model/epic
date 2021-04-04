b81d2d8ea1489b6cdc8190e721164cedf769ddd9
Matthias Frey <mf248@st-andrews.ac.uk>
2021-04-04 12:54:33 +0200

    update test

--- test/ellipse_orientation.f90
+++ test/ellipse_orientation.f90
@@ -2,6 +2,7 @@ program ellipse_orientation
     use hdf5
     use constants, only : pi
     use parcel_container
+    use ellipse
     use writer
     implicit none
 
@@ -10,8 +11,9 @@ program ellipse_orientation
     double precision :: extent(2) =  (/0.2, 0.2/)
     integer :: iter
     integer :: grid(2) = (/2, 2/)
-    double precision :: angle
+    double precision :: angle, B11, B12
     double precision, parameter :: lam = 3.0
+    logical :: failed = .false.
 
     n_parcels = 2
     call parcel_alloc(2)
@@ -23,25 +25,30 @@ program ellipse_orientation
 
         angle = dble(iter) * pi / 180.0d0
 
-        print *, angle
+        B11 = lam * cos(angle) ** 2 + 1.0 / lam * sin(angle) ** 2
 
-        ! b11
-        parcels%B(1, 1) = lam * cos(angle) ** 2 + 1.0 / lam * sin(angle) ** 2
+        B12 = 0.5 * (lam - 1.0 / lam) * sin(2.0 * angle)
 
-        ! b12
-        parcels%B(1, 2) = 0.5 * (lam - 1.0 / lam) * sin(2.0 * angle)
+        ! get_angle computes the angle in the first and fourth quadrant, i.e.,
+        ! -pi/2 <= get_angle <= pi/2
+        if (angle > pi / 2 .and. angle <= 3.0 * pi / 2) then
+            angle = angle - pi
+        else if (angle > 3.0 * pi / 2) then
+            angle = angle - 2 * pi
+        endif
 
+        failed = (failed .or. abs(angle - get_angle(B11, B12)) > 1.0e-13)
 
         call open_h5_file('ellipse_orientation.hdf5')
         call write_h5_scalar_step_attrib(iter, "t", dble(iter))
         call write_h5_scalar_step_attrib(iter, "dt", dble(iter))
 
 
-        print *, iter
+        parcels%B(1, 1) = B11
+        parcels%B(1, 2) = B12
 
         call write_h5_parcels(iter)
         call close_h5_file
-
     enddo
 
 
@@ -67,5 +74,11 @@ program ellipse_orientation
     call close_h5_file
 
     call parcel_dealloc()
+
+    if (failed) then
+        print *, 'Test ellipse_orientation: FAILED'
+    else
+        print *, 'Test ellipse_orientation: PASSED'
+    endif
 
 end program ellipse_orientation
