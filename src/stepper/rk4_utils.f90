7cf5db9c77ade1de69d90e6d221043e2e64d54d2
Steven Boeing <stevenboeing@localhost.localdomain>
2021-04-06 09:14:29 +0100

    Only apply get_B to 1:n_parcels

--- src/stepper/rk4_utils.f90
+++ src/stepper/rk4_utils.f90
@@ -1,6 +1,7 @@
 module rk4_utils
     use constants, only : max_num_parcels
     use ellipse, only : get_B22
+    use parcel_container
 
     implicit none
 
@@ -10,15 +11,14 @@ module rk4_utils
             double precision, intent(in) :: Bin(max_num_parcels, 2)
             double precision, intent(in) :: S(max_num_parcels, 4)
             double precision             :: Bout(max_num_parcels, 2)
-            double precision             :: B22(max_num_parcels)
-
-            B22 = get_B22(Bin(:, 1), Bin(:, 2))
 
             ! B11 = 2 * (dudx * B11 + dudy * B12)
-            Bout(:, 1) = 2.0 * (S(:, 1) * Bin(:, 1) + S(:, 2) * Bin(:, 2))
+            Bout(1:n_parcels, 1) = 2.0 * (S(1:n_parcels, 1) * Bin(1:n_parcels, 1) &
+                                 + S(1:n_parcels, 2) * Bin(1:n_parcels, 2))
 
             ! B12 = dvdx * B11 + dudy * B22
-            Bout(:, 2) = S(:, 3) * Bin(:, 1) + S(:, 2) * B22
+            Bout(1:n_parcels, 2) = S(1:n_parcels, 3) * Bin(1:n_parcels, 1) &
+                                 + S(1:n_parcels, 2) * get_B22(Bin(1:n_parcels, 1), Bin(1:n_parcels, 2))
 
         end function get_B
 
