dba913dc892ab019b759ebb7015e8218a8ecbaf4
Matthias Frey <mf248@st-andrews.ac.uk>
2021-04-07 16:08:58 +0200

    formatting deriv1d.f90

--- src/utils/deriv1d.f90
+++ src/utils/deriv1d.f90
@@ -1,156 +1,124 @@
+! =============================================================================
+!           Module to compute 1D derivatives of Fourier series
+! =============================================================================
 module deriv1d
+    use constants, only : pi
+    implicit none
 
-implicit none
+    contains
 
-contains
+        subroutine init_deriv(n, l, k)
+            integer,          intent(in)  :: n
+            double precision, intent(in)  :: l
+            double precision, intent(out) :: k(n)
+            double precision              :: sc
+            integer                       :: i
 
-!========================================================================
-subroutine init_deriv(n,l,k)
+            ! define wavenumber array:
+            if (l .ne. 0.d0) then
+                ! define wavenumbers:
+                sc = pi / l
+                do i = 1, n
+                    k(i) = sc * dble(i)
+                enddo
+            else
+                ! catastrophic end to run if wave number definition fails:
+                write(*,*) '**********************************************'
+                write(*,*) ' Wavenumber array definition not possible.'
+                write(*,*) ' Domain length equal to zero not allowed.'
+                write(*,*) ' STOPPING...'
+                write(*,*) '**********************************************'
+                stop
+            endif
+        end subroutine
 
-implicit none
 
- !Arguments declarations:
-integer:: n
-double precision:: l,k(n)
- !Local declarations:
-double precision,parameter:: pi=3.141592653589793238462643383279502884197169399375105820974944592307816d0
-double precision:: sc
-integer:: i
-!-----------------------------------------------------
+        ! Calculates m derivatives of length n for the array var(m,n)
+        ! and stores the result in der(m,n).
+        ! *** Both var and der are assumed to be Fourier ***
+        !   *** coeffiecients in their second argument ***
+        subroutine deriv(m, n, k, var, der)
+            integer,          intent(in)  :: m, n
+            double precision, intent(in)  :: k(n), var(m,n),
+            double precision, intent(out) :: der(m,n)
+            integer                       :: nw, np2, i, j, ic
+            double precision              :: ktmp
 
- !Define wavenumber array:
-if (l .ne. 0.d0) then
-   !Define wavenumbers:
-  sc=pi/l
-  do i=1,n
-    k(i)=sc*dble(i)
-  enddo
-else
-   !Catastrophic end to run if wave number definition fails:
-  write(*,*) '**********************************************'
-  write(*,*) ' Wavenumber array definition not possible.'
-  write(*,*) ' Domain length equal to zero not allowed.'
-  write(*,*) ' STOPPING...'
-  write(*,*) '**********************************************'
-  stop
-endif
+            nw = n / 2
+            np2 = n + 2
 
-return
-end subroutine
+            ! Carry out differentiation by wavenumber multiplication:
+            do j=1,m
+                der(j, 1) = 0.d0
+            enddo
 
-!==========================================================================
+            do i = 2, n - nw
+                ktmp = k(2*(i-1))
+                ic = np2 - i
+                do j = 1, m
+                    der(j, i) = -ktmp * var(j,ic)
+                    der(j,ic) =  ktmp * var(j ,i)
+                enddo
+            enddo
 
-subroutine deriv(m,n,k,var,der)
-! Calculates m derivatives of length n for the array var(m,n) 
-! and stores the result in der(m,n).
-! *** Both var and der are assumed to be Fourier ***
-!   *** coeffiecients in their second argument ***
+            if (mod(n,2) .eq. 0) then
+                ic=nw+1
+                do j = 1, m
+                    der(j,ic) = 0.d0
+                enddo
+            endif
+        end subroutine
 
-implicit none
 
- !Arguments declarations:
-integer:: m,n
-double precision:: k(n),var(m,n),der(m,n)
- !Local declarations:
-integer:: nw,np2,i,j,ic
-double precision:: ktmp
-!---------------------------------------------------------------
+        ! Calculates m derivatives of length n for the array var(m,n)
+        ! and stores the result in der(m,n), where var is represented by a
+        ! cosine series. Note der is therefore a sine series.
+        ! *** Both var and der are assumed to be Fourier ***
+        !   *** coeffiecients in their second argument ***
+        subroutine deriv_c(m, n, k, var, der)
+            integer,          intent(in)  :: m, n
+            double precision, intent(in)  :: k(n), var(m,0:n)
+            double precision, intent(out) :: der(m,n)
+            integer                       :: i, j
+            double precision              :: ktmp
 
-nw=n/2
-np2=n+2
- !Carry out differentiation by wavenumber multiplication:
-do j=1,m
-  der(j,1)=0.d0
-enddo
+            ! Carry out differentiation by wavenumber multiplication:
+            do i = 1, n-1
+                ktmp = -k(i)
+                do j = 1, m
+                    der(j,i) = ktmp * var(j,i)
+                enddo
+            enddo
 
-do i=2,n-nw
-  ktmp=k(2*(i-1))
-  ic=np2-i
-  do j=1,m
-    der(j, i)=-ktmp*var(j,ic)
-    der(j,ic)= ktmp*var(j ,i)
-  enddo
-enddo
+            do j = 1, m
+                der(j, n) = 0.d0
+            enddo
+        end subroutine
 
-if (mod(n,2) .eq. 0) then
-  ic=nw+1
-  do j=1,m
-    der(j,ic)=0.d0
-  enddo
-endif
 
-return
-end subroutine
+        ! Calculates m derivatives of length n for the array var(m,n)
+        ! and stores the result in der(m,n), where var is represented by a
+        ! sine series. Note der is therefore a cosine series.
+        ! *** Both var and der are assumed to be Fourier ***
+        !   *** coeffiecients in their second argument ***
+        subroutine deriv_s(m, n, k, var, der)
+            integer,          intent(in)  :: m,n
+            double precision, intent(in)  :: k(n), var(m,n),
+            double precision, intent(out) :: der(m, 0:n)
+            integer                       :: i, j
+            double precision              :: ktmp
 
-!==========================================================================
+            ! Carry out differentiation by wavenumber multiplication:
+            do i = 1, n-1
+                ktmp = k(i)
+                do j = 1, m
+                    der(j,i) = ktmp * var(j,i)
+                enddo
+            enddo
 
-subroutine deriv_c(m,n,k,var,der)
-! Calculates m derivatives of length n for the array var(m,n) 
-! and stores the result in der(m,n), where var is represented by a 
-! cosine series. Note der is therefore a sine series.
-! *** Both var and der are assumed to be Fourier ***
-!   *** coeffiecients in their second argument ***
-
-implicit none
-
- !Arguments declarations:
-integer:: m,n
-double precision:: k(n),var(m,0:n),der(m,n)
- !Local declarations:
-integer:: i,j
-double precision:: ktmp
-!------------------------------------------------------------------
-
- !Carry out differentiation by wavenumber multiplication:
-do i=1,n-1
-  ktmp=-k(i)
-  do j=1,m
-    der(j,i)=ktmp*var(j,i)
-  enddo
-enddo
-
-do j=1,m
-  der(j,n)=0.d0
-enddo
-
-return
-end subroutine
-
-!===================================================================
-
-subroutine deriv_s(m,n,k,var,der)
-! Calculates m derivatives of length n for the array var(m,n) 
-! and stores the result in der(m,n), where var is represented by a 
-! sine series. Note der is therefore a cosine series.
-! *** Both var and der are assumed to be Fourier ***
-!   *** coeffiecients in their second argument ***
-
-implicit none
-
- !Arguments declarations:
-integer:: m,n
-double precision:: k(n),var(m,n),der(m,0:n)
- !Local declarations:
-integer:: i,j
-double precision:: ktmp
-!------------------------------------------------------------------
-
- !Carry out differentiation by wavenumber multiplication:
-do i=1,n-1
-  ktmp=k(i)
-  do j=1,m
-    der(j,i)=ktmp*var(j,i)
-  enddo
-enddo
-
-do j=1,m
-  der(j,0)=0.d0
-  der(j,n)=0.d0
-enddo
-
-return
-end subroutine
-
-!===================================================================
-
+            do j = 1, m
+                der(j, 0) = 0.d0
+                der(j, n) = 0.d0
+            enddo
+        end subroutine
 end module
