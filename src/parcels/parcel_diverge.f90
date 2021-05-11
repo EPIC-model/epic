f579c46cb04c8aeeb36c540bb79497ef0d84f6b4
Steven Boeing <stevenboeing@localhost.localdomain>
2021-05-11 22:22:50 +0100

    Naming for prefactor

--- src/parcels/parcel_diverge.f90
+++ src/parcels/parcel_diverge.f90
@@ -197,9 +197,9 @@ module parcel_diverge
         enddo
     end subroutine apply_diverge
 
-    subroutine apply_gradient(volg,gradient_pref)
+    subroutine apply_gradient(volg,prefactor)
         double precision, intent(in) :: volg(-1:, 0:, :)
-        double precision, intent(in) :: gradient_pref
+        double precision, intent(in) :: prefactor
         double precision             :: phi(0:nz,0:nx-1)
         double precision             :: weights(ngp)
         integer                      :: n, is(ngp), js(ngp)
@@ -214,12 +214,12 @@ module parcel_diverge
             call trilinear(parcels%position(n, :), is, js, weights)
 
             parcels%position(n, 1) = parcels%position(n, 1)    &
-            - gradient_pref*dx(1)*(weights(2)+weights(1))*(phi(js(2), is(2))-phi(js(1), is(1)))  &
-            - gradient_pref*dx(1)*(weights(4)+weights(3))*(phi(js(4), is(4))-phi(js(3), is(3)))
+            - prefactor*dx(1)*(weights(2)+weights(1))*(phi(js(2), is(2))-phi(js(1), is(1)))  &
+            - prefactor*dx(1)*(weights(4)+weights(3))*(phi(js(4), is(4))-phi(js(3), is(3)))
 
             parcels%position(n, 2) = parcels%position(n, 2)             &
-            - gradient_pref*dx(2)*(weights(3)+weights(1))*(phi(js(3), is(3))-phi(js(1), is(1))) &
-            - gradient_pref*dx(2)*(weights(4)+weights(2))*(phi(js(4), is(4))-phi(js(2), is(2)))
+            - prefactor*dx(2)*(weights(3)+weights(1))*(phi(js(3), is(3))-phi(js(1), is(1))) &
+            - prefactor*dx(2)*(weights(4)+weights(2))*(phi(js(4), is(4))-phi(js(2), is(2)))
 
             call apply_periodic_bc(parcels%position(n, :))
         enddo
