2975e3348b05f6ba10140be3714ec14f6e8a77d5
Matthias Frey <mf248@st-andrews.ac.uk>
2021-05-11 09:22:04 +0200

    use constants

--- src/utils/options.f90
+++ src/utils/options.f90
@@ -2,7 +2,7 @@
 ! This module contains global options that can be set at runtime by the user.
 ! =============================================================================
 module options
-    use constants, only : two, pi
+    use constants, only : zero, one, two, pi, five
     implicit none
     !
     ! global options
@@ -42,11 +42,11 @@ module options
         logical          :: is_random    = .false.        ! random parcel initialization
         integer          :: seed         = 42             ! seed of random initialization
         logical          :: is_elliptic  = .true.         ! use elliptic model
-        double precision :: lambda       = 5.0            ! max. ellipse aspect ratio a/b
+        double precision :: lambda       = five           ! max. ellipse aspect ratio a/b
         integer          :: split_freq   = 1              ! split frequency, 1 = call split subroutine every step
         character(15)    :: merge_type   = 'bi-geometric' ! merge method in use (other option: 'optimal')
         integer          :: merge_freq   = 1              ! merge frequency, 1 = call merge subroutine every step
-        double precision :: vfraction    = 36.0           ! volume fraction in merge
+        double precision :: vfraction    = 36.0d0         ! volume fraction in merge
         integer          :: diverge_freq = 1              ! diverge frequency, 1 = call diverge module every step
     end type parcel_info_type
 
@@ -59,8 +59,8 @@ module options
 
     ! time limit
     type time_info_type
-        double precision :: limit       = 0.0       ! time limit
-        double precision :: dt          = 0.0       ! time step
+        double precision :: limit       = zero       ! time limit
+        double precision :: dt          = zero       ! time step
         logical          :: is_adaptive = .false.
     end type time_info_type
 
@@ -75,9 +75,9 @@ module options
     ! TaylorGreen flow options
     !
     type flow_type
-        double precision :: amp(2) = 1.0    ! amplitudes
-        double precision :: freq(2) = 1.0   ! frequencies
-        double precision :: phase(2) = 1.0  ! phase shift
+        double precision :: amp(2) = one    ! amplitudes
+        double precision :: freq(2) = one   ! frequencies
+        double precision :: phase(2) = one  ! phase shift
     end type flow_type
 
     type(flow_type) :: flow
