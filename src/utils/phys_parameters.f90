53fe4e6feb78cd7674f2dc6de7aa583fe8aba59a
Matthias Frey <mf248@st-andrews.ac.uk>
2021-06-18 10:56:02 +0200

    update physical constants

--- src/utils/phys_parameters.f90
+++ src/utils/phys_parameters.f90
@@ -5,22 +5,10 @@ module phys_parameters
     use phys_constants
     implicit none
 
-    ![g/m**3] surface saturation humidity
-    double precision, parameter :: q0_c = 0.0d0 ! FIXME
-
-    ![J/g] latent heat of condensation
-    double precision, parameter :: L_c = 0.0d0 ! FIXME
-
-    ![J/(g*K)] specific heat at constant pressure
-    double precision, parameter :: cp_c = 1.0d0 ! FIXME
-
     ![m] inverse condensation scale-height
     double precision, parameter :: lam_c = 0.0d0 ! FIXME
 
-    ![K] liquid-water potential temperature reference
-    double precision, parameter :: theta_c = 1.0d0 ! FIXME
-
     ![] see equation (5) of MPIC paper
-    double precision, parameter :: glat_c = gravity * L_c / (cp_c * theta_c)
+    double precision, parameter :: glat = gravity * L_c * h_0 / (c_p * theta_l0)
 
 end module phys_parameters
