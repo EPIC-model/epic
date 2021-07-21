e70576208f2501cbead893215c77c8019a19022a
Matthias Frey <mf248@st-andrews.ac.uk>
2021-07-21 17:40:28 +0200

    update h5_fields.py

--- python-scripts/tools/h5_fields.py
+++ python-scripts/tools/h5_fields.py
@@ -20,7 +20,41 @@ class h5fields:
         self._h5file = h5py.File(fname, 'w')
 
 
-    def add_box(self, origin, extent, ncells):
+    def add_fields(self, origin, extent, fields):
+        """
+        Write all fields provided by the dictionary.
+
+        Parameters
+        ----------
+        origin : tuple of floats (length 2)
+            The origin of the domain (horizontal, vertical).
+        extent : tuple of floats (length 2)
+            The extent of the box (horizontal, vertical).
+        fields : dict
+            All fields to be added. They keys are the field
+            names and the values are the data (horizontal, vertical).
+        """
+        if not isinstance(fields, dict):
+            TypeError('Argument not a dictionary.')
+
+        shape = None
+
+        for key, values in fields.items():
+            if shape is None:
+                shape = np.shape(values)
+            elif not shape == np.shape(values):
+                raise RuntimeError("Field '" + key + "' must have shape " + str(shape))
+
+            self._add_dataset(name=key, data=values)
+
+        ncells = (shape[0], shape[1]-1)
+        self._add_box(origin, extent, ncells)
+
+
+    def close(self):
+        self._h5file.close()
+
+    def _add_box(self, origin, extent, ncells):
         """
         Box dictionary:
 
@@ -52,15 +86,10 @@ class h5fields:
         group.attrs['ncells'] = ncells
 
 
-    def add_dataset(self, name, data, dtype='f'):
+    def _add_dataset(self, name, data, dtype='f'):
         """
         Add a field dataset. The horizontal dimension is the
         leading dimension (horizontal x vertical).
         """
         data = np.asarray(data)
-        data = data.transpose()
         self._h5file.create_dataset(name=name, dtype=dtype, data=data)
-
-
-    def close(self):
-        self._h5file.close()
