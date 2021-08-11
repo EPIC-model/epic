dbe5c9ca6062b4a437e742409d424a1a977711ec
Matthias Frey <mf248@st-andrews.ac.uk>
2021-08-11 09:35:24 +0200

    all tests with 3 parcels

--- unit-tests/test_nearest_2.f90
+++ unit-tests/test_nearest_2.f90
@@ -19,7 +19,7 @@ program test_nearest_2
     use parcel_nearest
     implicit none
 
-    logical              :: failed = .false.
+    logical              :: passed = .true.
     integer, allocatable :: isma(:)
     integer, allocatable :: ibig(:)
     integer              :: n_merge, n
@@ -42,6 +42,7 @@ program test_nearest_2
 
     call permute_generate(n_parcels)
 
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     !
     !   (3a)
     !
@@ -51,36 +52,163 @@ program test_nearest_2
 
         call find_nearest(isma, ibig, n_merge)
 
-        failed = (failed .or. (n_merge .ne. 2))
+        passed = (passed .and. (n_merge == 2))
 
-        ! A --> B
-        failed = (failed .or. ((isma(1) .ne. ordering(1)) .or. (ibig(1) .ne. ordering(2))))
+        ! B is in ibig since A --> B and C --> B
+        passed = (passed .and. &
+                    (ibig(1) == ordering(2)) .and. (ibig(2) == ordering(2)))
 
-        ! C --> B
-        failed = (failed .or. ((isma(2) .ne. ordering(3)) .or. (ibig(2) .ne. ordering(2))))
-
-        print *, ordering, isma(1), ordering(1)
-!         stop
+        ! A and C are in isma
+        if (ordering(1) < ordering(3)) then
+            passed = (passed .and. &
+                        (isma(1) == ordering(1)) .and. (isma(2) == ordering(3)))
+        else
+            passed = (passed .and. &
+                        (isma(1) == ordering(3)) .and. (isma(2) == ordering(1)))
+        endif
     enddo
 
-    call print_result_logical('Test nearest algorithm: A(1) <-> B(1) <- C(1)', failed)
+    call print_result_logical('Test nearest algorithm: A(1) <-> B(1) <-  C(1)', passed)
 
-    stop
-
-    failed = .false.
-
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     !
     !   (3b)
     !
-!     call parcel_setup_3b
+    passed = .true.
 
-    call find_nearest(isma, ibig, n_merge)
+    do n = 1, n_permutes
+        ordering = permutes(n, :)
+        call parcel_setup_3b(ordering)
 
-    failed = (n_merge .ne. 1)
-    failed = (failed .or. ((isma(1) .ne. 1) .or. (ibig(1) .ne. 2)))
-    call print_result_logical('Test nearest algorithm: A(1)  -> B(1) -> C(2)', failed)
+        call find_nearest(isma, ibig, n_merge)
 
-    deallocate(permutes)
+        passed = (passed .and. (n_merge == 1))
+
+        ! A --> B
+        passed = (passed .and. (isma(1) == ordering(1)))
+        passed = (passed .and. (ibig(1) == ordering(2)))
+    enddo
+
+    call print_result_logical('Test nearest algorithm: A(1)  -> B(1)  -> C(2)', passed)
+
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    !
+    !   (3c)
+    !
+    passed = .true.
+
+    do n = 1, n_permutes
+        ordering = permutes(n, :)
+        call parcel_setup_3c(ordering)
+
+        call find_nearest(isma, ibig, n_merge)
+
+        passed = (passed .and. (n_merge == 2))
+
+        ! B is in ibig since A --> B and C --> B
+        passed = (passed .and. &
+                    (ibig(1) == ordering(2)) .and. (ibig(2) == ordering(2)))
+
+        ! A and C are in isma
+        if (ordering(1) < ordering(3)) then
+            passed = (passed .and. &
+                        (isma(1) == ordering(1)) .and. (isma(2) == ordering(3)))
+        else
+            passed = (passed .and. &
+                        (isma(1) == ordering(3)) .and. (isma(2) == ordering(1)))
+        endif
+
+    enddo
+
+    call print_result_logical('Test nearest algorithm: A(1)  -> B(2) <-> C(2)', passed)
+
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    !
+    !   (3d)
+    !
+    passed = .true.
+
+    do n = 1, n_permutes
+        ordering = permutes(n, :)
+        call parcel_setup_3d(ordering)
+
+        call find_nearest(isma, ibig, n_merge)
+
+        passed = (passed .and. (n_merge == 2))
+
+        ! B is in ibig since A --> B and C --> B
+        passed = (passed .and. &
+                    (ibig(1) == ordering(2)) .and. (ibig(2) == ordering(2)))
+
+        ! A and C are in isma
+        if (ordering(1) < ordering(3)) then
+            passed = (passed .and. &
+                        (isma(1) == ordering(1)) .and. (isma(2) == ordering(3)))
+        else
+            passed = (passed .and. &
+                        (isma(1) == ordering(3)) .and. (isma(2) == ordering(1)))
+        endif
+
+    enddo
+
+    call print_result_logical('Test nearest algorithm: A(1)  -> B(2) <-  C(1)', passed)
+
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    !
+    !   (3e)
+    !
+    passed = .true.
+
+    do n = 1, n_permutes
+        ordering = permutes(n, :)
+        call parcel_setup_3e(ordering)
+
+        call find_nearest(isma, ibig, n_merge)
+
+        passed = (passed .and. (n_merge == 2))
+
+        ! B is in ibig since A --> B and C --> B
+        passed = (passed .and. &
+                    (ibig(1) == ordering(2)) .and. (ibig(2) == ordering(2)))
+
+        ! A and C are in isma
+        if (ordering(1) < ordering(3)) then
+            passed = (passed .and. &
+                        (isma(1) == ordering(1)) .and. (isma(2) == ordering(3)))
+        else
+            passed = (passed .and. &
+                        (isma(1) == ordering(3)) .and. (isma(2) == ordering(1)))
+        endif
+
+    enddo
+
+    call print_result_logical('Test nearest algorithm: A(1)  -> B(3) <-  C(2)', passed)
+
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    !
+    !   (3f)
+    !
+    passed = .true.
+
+    do n = 1, n_permutes
+        ordering = permutes(n, :)
+        call parcel_setup_3f(ordering)
+
+        call find_nearest(isma, ibig, n_merge)
+
+        passed = (passed .and. (n_merge == 1))
+
+        ! B is in ibig
+        passed = (passed .and. (ibig(1) == ordering(2)))
+
+        ! A is in isma
+        passed = (passed .and. (isma(1) == ordering(1)))
+    enddo
+
+    call print_result_logical('Test nearest algorithm: A(1)  -> B(2)  -> C(3)', passed)
+
+
+    call permute_dealloc
 
     contains
 
@@ -98,21 +226,80 @@ program test_nearest_2
             parcels%position(p(3), 2) = zero
             parcels%volume(p(3)) = 0.1d0 * pi
         end subroutine parcel_setup_3a
-!
-!         subroutine parcel_setup_3b
-!             integer, intent(in) :: p(3)
-!             parcels%position(p(1), 1) = -0.2d0
-!             parcels%position(p(1), 2) = zero
-!             parcels%volume(p(1)) = 0.1d0 * pi
-!
-!             parcels%position(p(2), 1) = 0.0d0
-!             parcels%position(p(2), 2) = zero
-!             parcels%volume(p(2)) = 0.1d0 * pi
-!
-!             parcels%position(p(3), 1) = 0.1d0
-!             parcels%position(p(3), 2) = zero
-!             parcels%volume(p(3)) = 0.12d0 * pi
-!         end subroutine parcel_setup_3b
 
+        subroutine parcel_setup_3b(p)
+            integer, intent(in) :: p(3)
+            parcels%position(p(1), 1) = -0.2d0
+            parcels%position(p(1), 2) = zero
+            parcels%volume(p(1)) = 0.1d0 * pi
+
+            parcels%position(p(2), 1) = 0.0d0
+            parcels%position(p(2), 2) = zero
+            parcels%volume(p(2)) = 0.1d0 * pi
+
+            parcels%position(p(3), 1) = 0.1d0
+            parcels%position(p(3), 2) = zero
+            parcels%volume(p(3)) = 0.12d0 * pi
+        end subroutine parcel_setup_3b
+
+        subroutine parcel_setup_3c(p)
+            integer, intent(in) :: p(3)
+            parcels%position(p(1), 1) = -0.2d0
+            parcels%position(p(1), 2) = zero
+            parcels%volume(p(1)) = 0.1d0 * pi
+
+            parcels%position(p(2), 1) = 0.0d0
+            parcels%position(p(2), 2) = zero
+            parcels%volume(p(2)) = 0.12d0 * pi
+
+            parcels%position(p(3), 1) = 0.1d0
+            parcels%position(p(3), 2) = zero
+            parcels%volume(p(3)) = 0.12d0 * pi
+        end subroutine parcel_setup_3c
+
+        subroutine parcel_setup_3d(p)
+            integer, intent(in) :: p(3)
+            parcels%position(p(1), 1) = -0.2d0
+            parcels%position(p(1), 2) = zero
+            parcels%volume(p(1)) = 0.1d0 * pi
+
+            parcels%position(p(2), 1) = 0.0d0
+            parcels%position(p(2), 2) = zero
+            parcels%volume(p(2)) = 0.12d0 * pi
+
+            parcels%position(p(3), 1) = 0.2d0
+            parcels%position(p(3), 2) = zero
+            parcels%volume(p(3)) = 0.1d0 * pi
+        end subroutine parcel_setup_3d
+
+        subroutine parcel_setup_3e(p)
+            integer, intent(in) :: p(3)
+            parcels%position(p(1), 1) = -0.2d0
+            parcels%position(p(1), 2) = zero
+            parcels%volume(p(1)) = 0.1d0 * pi
+
+            parcels%position(p(2), 1) = 0.0d0
+            parcels%position(p(2), 2) = zero
+            parcels%volume(p(2)) = 0.14d0 * pi
+
+            parcels%position(p(3), 1) = 0.2d0
+            parcels%position(p(3), 2) = zero
+            parcels%volume(p(3)) = 0.12d0 * pi
+        end subroutine parcel_setup_3e
+
+        subroutine parcel_setup_3f(p)
+            integer, intent(in) :: p(3)
+            parcels%position(p(1), 1) = -0.2d0
+            parcels%position(p(1), 2) = zero
+            parcels%volume(p(1)) = 0.1d0 * pi
+
+            parcels%position(p(2), 1) = 0.0d0
+            parcels%position(p(2), 2) = zero
+            parcels%volume(p(2)) = 0.12d0 * pi
+
+            parcels%position(p(3), 1) = 0.2d0
+            parcels%position(p(3), 2) = zero
+            parcels%volume(p(3)) = 0.14d0 * pi
+        end subroutine parcel_setup_3f
 
 end program test_nearest_2
