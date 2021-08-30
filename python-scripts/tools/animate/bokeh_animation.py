f5a98c7c6b7b14c8cbfc0c4cb69f16190d206964
Matthias Frey <mf248@st-andrews.ac.uk>
2021-08-30 16:11:39 +0200

    improve animation

--- python-scripts/tools/animate/bokeh_animation.py
+++ python-scripts/tools/animate/bokeh_animation.py
@@ -21,18 +21,45 @@ class BokehAnimation:
         nsteps = self.h5reader.get_num_steps()
         os.mkdir("temp-movie")
 
+        tmin = kwargs.pop('tmin', None)
+        tmax = kwargs.pop('tmax', None)
+
+        if tmin is None:
+            tmin = 0.0
+
+        if tmax is None:
+            t = self.h5reader.get_step_attribute(nsteps - 2, 't')
+            dt = self.h5reader.get_step_attribute(nsteps - 2, 'dt')
+            tmax = t + 10 * dt
+
         bar = progressbar.ProgressBar(maxval=nsteps).start()
+        i = 0
         for step in range(nsteps):
+
+            t = self.h5reader.get_step_attribute(step, 't')
+            if t < tmin:
+                continue
+
+            if t > tmax:
+                break
+
             if coloring == 'aspect-ratio':
                 vmin = 1.0
                 vmax = self.h5reader.get_parcel_option('lambda')
+            elif coloring == 'vol-distr':
+                extent = self.h5reader.get_box_extent()
+                ncells = self.h5reader.get_box_ncells()
+                vcell = np.prod(extent / ncells)
+                vmin = vcell / self.h5reader.get_parcel_option('min_vratio')
+                vmax = vcell / self.h5reader.get_parcel_option('max_vratio')
             else:
                 vmin, vmax = self.h5reader.get_dataset_min_max(coloring)
 
             graph = _bokeh_plot_parcels(self.h5reader, step, coloring, vmin, vmax, **kwargs)
 
-            export_png(graph, filename = "temp-movie/movie.%05d.png"%step)
+            export_png(graph, filename = "temp-movie/movie.%05d.png"%i)
             bar.update(step+1)
+            i = i + 1
         bar.finish()
 
     def save(self, fname):
