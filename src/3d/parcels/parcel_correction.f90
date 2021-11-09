554495eb69712e049a1ab0b5f24ec9bef10cd7c4
Matthias Frey <mf248@st-andrews.ac.uk>
2021-11-09 17:14:45 +0000

    gradient correction

--- src/3d/parcels/parcel_correction.f90
+++ src/3d/parcels/parcel_correction.f90
@@ -18,7 +18,7 @@ module parcel_correction
     use omp_lib
 
     use constants
-    use parameters, only : vcelli, nx, nz, dx, dxi
+    use parameters, only : vcelli, nx, ny, nz, dx, dxi
 
     use parcel_container
 
@@ -183,12 +183,12 @@ module parcel_correction
 
     end subroutine apply_laplace
 
-    subroutine apply_gradient(prefactor, max_compression) !FIXME
+    subroutine apply_gradient(prefactor, max_compression)
         double precision, intent(in) :: prefactor
         double precision, intent(in) :: max_compression
         double precision             :: phi(0:nz, 0:ny-1, 0:nx-1)
         double precision             :: weights(ngp)
-        double precision             :: xs, ys, zs, xf, yf, lim_x, lim_y, lim_z
+        double precision             :: xs, ys, zs, xf, yf, zf, lim_x, lim_y, lim_z
         integer                      :: n, is(ngp), js(ngp), ks(ngp)
 !
         call start_timer(grad_corr_timer)
@@ -204,34 +204,54 @@ module parcel_correction
 
             call trilinear(parcels%position(n, :), is, js, ks, weights)
 
-            xf = weights(2) + weights(4) ! fractional position along x
-            yf = weights(3) + weights(4) ! fractional position along y
+            xf = weights(2) + weights(4) + weights(6) + weights(8) ! fractional position along x
+            yf = weights(3) + weights(4) + weights(7) + weights(8) ! fractional position along y
+            zf = weights(5) + weights(6) + weights(7) + weights(8) ! fractional position along z
 
-! FIXME
-!             xs= - prefactor * dx(1) * xf * (one - xf) * (&
-!                         (one - yf) * (phi(js(2), is(2)) - phi(js(1), is(1)))  &
-!                       +       (yf) * (phi(js(4), is(4)) - phi(js(3), is(3))))
-!
-!             lim_x = max_compression * dx(1) * xf * (one - xf)
-!
-!             xs = max(-lim_x, min(xs, lim_x))
-!
-!             ys = - prefactor * dx(2) * yf * (one - yf) * (&
-!                          (one-xf) * (phi(js(3), is(3)) - phi(js(1), is(1))) &
-!                        +     (xf) * (phi(js(4), is(4)) - phi(js(2), is(2))))
-!
-!             lim_y = max_compression * dx(2) * yf * (one - yf)
-!
-!             ys = max(-lim_y, min(ys, lim_y))
-!
-!
-!             zs = - prefactor * dx(3) * zf * (one - zf) * (&
-!                          (one-xf) * (phi(js(3), is(3)) - phi(js(1), is(1))) &
-!                        +     (xf) * (phi(js(4), is(4)) - phi(js(2), is(2))))
-!
-!             lim_z = max_compression * dx(3) * zf * (one - zf)
-!
-!             zs = max(-lim_z, min(zs, lim_z))
+            ! l   ks(l) js(l) is(l)
+            ! 1   k     j     i
+            ! 2   k     j     i+1
+            ! 3   k     j+1   i
+            ! 4   k     j+1   i+1
+            ! 5   k+1   j     i
+            ! 6   k+1   j     i+1
+            ! 7   k+1   j+1   i
+            ! 8   k+1   j+1   i+1
+            lim_x = dx(1) * xf * (one - xf)
+            xs = - prefactor * lim_x * (&
+                        (one - zf) * (&
+                                (one - yf) * (phi(ks(2), js(2), is(2)) - phi(ks(1), js(1), is(1)))  &
+                              +       (yf) * (phi(ks(4), js(4), is(4)) - phi(ks(3), js(3), is(3)))) &
+                       +      (zf) * (&
+                                (one - yf) * (phi(ks(6), js(6), is(6)) - phi(ks(5), js(5), is(5)))  &
+                              +       (yf) * (phi(ks(8), js(8), is(8)) - phi(ks(7), js(7), is(7)))))
+
+            lim_x = lim_x * max_compression
+            xs = max(-lim_x, min(xs, lim_x))
+
+            lim_y = dx(2) * yf * (one - yf)
+            ys = - prefactor * lim_y * (&
+                        (one - zf) * (&
+                                (one - xf) * (phi(ks(3), js(3), is(3)) - phi(ks(1), js(1), is(1)))  &
+                              +       (xf) * (phi(ks(4), js(4), is(4)) - phi(ks(2), js(2), is(2)))) &
+                       +      (zf) * (&
+                                (one - xf) * (phi(ks(7), js(7), is(7)) - phi(ks(5), js(5), is(5)))  &
+                              +       (xf) * (phi(ks(8), js(8), is(8)) - phi(ks(6), js(6), is(6)))))
+
+            lim_y = lim_y * max_compression
+            ys = max(-lim_y, min(ys, lim_y))
+
+            lim_z = dx(3) * zf * (one - zf)
+            zs = - prefactor * lim_z * (&
+                        (one - xf) * (&
+                                (one - yf) * (phi(ks(5), js(5), is(5)) - phi(ks(1), js(1), is(1)))  &
+                              +       (yf) * (phi(ks(7), js(7), is(7)) - phi(ks(3), js(3), is(3)))) &
+                       +      (xf) * (&
+                                (one - yf) * (phi(ks(6), js(6), is(6)) - phi(ks(2), js(2), is(2)))  &
+                              +       (yf) * (phi(ks(8), js(8), is(8)) - phi(ks(4), js(4), is(4)))))
+
+            lim_z = lim_z * max_compression
+            zs = max(-lim_z, min(zs, lim_z))
 
             parcels%position(n, 1) = parcels%position(n, 1) + xs
             parcels%position(n, 2) = parcels%position(n, 2) + ys
