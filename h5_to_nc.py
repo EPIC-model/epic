71bbfc0ce6f25919eace1751cc40a691f04f453a
Steven Boeing <stevenboeing@localhost.localdomain>
2021-12-17 12:58:40 +0000

    Cosmetic changes to h5 to nc script

--- h5_to_nc.py
+++ h5_to_nc.py
@@ -4,15 +4,14 @@ import os.path
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('input_file_name',type=str)
+parser.add_argument("input_file_name", type=str)
 args = parser.parse_args()
-input_file_name=args.input_file_name
+input_file_name = args.input_file_name
 
-file_ext = os.path.splitext(input_file_name)[1]
-file_root = os.path.splitext(input_file_name)[0]
+file_root, file_ext = os.path.splitext(input_file_name)
 
-if not (file_ext=='.hdf5'):
+if not (file_ext == ".hdf5"):
     raise argparse.ArgumentTypeError('Argument filename must end on ".hdf5"')
 
-ds=xr.open_dataset(input_file_name)
-ds.to_netcdf(file_root+'.nc')
+ds = xr.open_dataset(input_file_name)
+ds.to_netcdf(file_root + ".nc")
