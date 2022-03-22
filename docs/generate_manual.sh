81f4f448960f14aefdc93725d3099896a50463dc
Matthias Frey <mf248@st-andrews.ac.uk>
2022-03-22 14:12:24 +0000

    add badges

--- docs/generate_manual.sh
+++ docs/generate_manual.sh
@@ -1,5 +1,12 @@
 #!/bin/env bash
 
+version=$(sed -nre 's/AC_INIT\(\[epic\], \[([0-9]+\.[0-9]+\.[0-9]+)\], (.*)\)/\1/p' ../configure.ac)
+
 cd manual
+
+sed -i "s:@VERSION@:$version:g" intro.adoc
+
 asciidoctor intro.adoc --destination-dir=../html
 asciidoctor-pdf intro.adoc --destination-dir=../pdf
+
+sed -i "s:$version:@VERSION@:g" intro.adoc
