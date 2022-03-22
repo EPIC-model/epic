b40d50c3e09b0d11719c2aadb1b088f82f64a764
Matthias Frey <mf248@st-andrews.ac.uk>
2022-03-22 14:40:28 +0000

    update intro.html

--- docs/generate_manual.sh
+++ docs/generate_manual.sh
@@ -7,6 +7,9 @@ cd manual
 sed -i "s:@VERSION@:$version:g" intro.adoc
 
 asciidoctor intro.adoc --destination-dir=../html
-asciidoctor-pdf intro.adoc --destination-dir=../pdf
+
+# 22 March 2022
+# https://discuss.asciidoctor.org/Diagrams-referenced-by-URL-in-PDF-td4623.html
+asciidoctor-pdf -a allow-uri-read intro.adoc --destination-dir=../pdf
 
 sed -i "s:$version:@VERSION@:g" intro.adoc
