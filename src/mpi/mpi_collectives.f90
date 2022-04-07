db63dcd4de426ae26dd8ddc1b322de21d891b8ab
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-07 15:09:01 +0100

    fix compilation errors

--- src/mpi/mpi_collectives.f90
+++ src/mpi/mpi_collectives.f90
@@ -2,49 +2,49 @@ module mpi_collectives
     use mpi_communicator
     implicit none
 
-    interface mpi_non_blocking_reduce
-        module procedure :: mpi_double_ireduce
-        module procedure :: mpi_integer_ireduce
-    end interface mpi_non_blocking_reduce
-
-    interface mpi_blocking_reduce
-        module procedure :: mpi_double_reduce
-        module procedure :: mpi_integer_reduce
-    end interface mpi_blocking_reduce
+!     interface mpi_non_blocking_reduce
+!         module procedure :: mpi_double_ireduce
+!         module procedure :: mpi_integer_ireduce
+!     end interface mpi_non_blocking_reduce
+!
+!     interface mpi_blocking_reduce
+!         module procedure :: mpi_double_reduce
+!         module procedure :: mpi_integer_reduce
+!     end interface mpi_blocking_reduce
 
     contains
-        subroutine mpi_double_ireduce(sendbuf, recvbuf, op)
-            double precision,  intent(in),  asynchronous :: sendbuf(..)
-            double precision,  intent(out), asynchronous :: recvbuf(..)
-            type(MPI_Op),      intent(in)                :: op
-            type(MPI_Request)                            :: request
-!             call MPI_Ireduce(sendbuf, recvbuf, size(recvbuf), MPI_DOUBLE, &
-!                              op, 0, comm, request, mpi_err)
-        end subroutine mpi_double_ireduce
-
-        subroutine mpi_integer_ireduce(sendbuf, recvbuf, op)
-            integer,           intent(in),  asynchronous :: sendbuf(..)
-            integer,           intent(out), asynchronous :: recvbuf(..)
-            type(MPI_Op),      intent(in)                :: op
-            type(MPI_Request)                            :: request
-!             call MPI_Ireduce(sendbuf, recvbuf, size(recvbuf), MPI_INT, &
-!                              op, 0, comm, request, mpi_err)
-        end subroutine mpi_integer_ireduce
-
-        subroutine mpi_double_reduce(sendbuf, recvbuf, op)
-            double precision, intent(in)  :: sendbuf(..)
-            double precision, intent(out) :: recvbuf(..)
-            type(MPI_Op),     intent(in)  :: op
-            call MPI_Reduce(sendbuf, recvbuf, size(recvbuf), MPI_DOUBLE, &
-                            op, 0, comm, mpi_err)
-        end subroutine mpi_double_reduce
-
-        subroutine mpi_integer_reduce(sendbuf, recvbuf, op)
-            integer,      intent(in)  :: sendbuf(..)
-            integer,      intent(out) :: recvbuf(..)
-            type(MPI_Op), intent(in)  :: op
-            call MPI_Reduce(sendbuf, recvbuf, size(recvbuf), MPI_INT, &
-                            op, 0, comm, mpi_err)
-        end subroutine mpi_integer_reduce
+!         subroutine mpi_double_ireduce(sendbuf, recvbuf, op)
+!             double precision,  intent(in),  asynchronous :: sendbuf(..)
+!             double precision,  intent(out), asynchronous :: recvbuf(..)
+!             type(MPI_Op),      intent(in)                :: op
+!             type(MPI_Request)                            :: request
+! !             call MPI_Ireduce(sendbuf, recvbuf, size(recvbuf), MPI_DOUBLE, &
+! !                              op, 0, comm, request, mpi_err)
+!         end subroutine mpi_double_ireduce
+!
+!         subroutine mpi_integer_ireduce(sendbuf, recvbuf, op)
+!             integer,           intent(in),  asynchronous :: sendbuf(..)
+!             integer,           intent(out), asynchronous :: recvbuf(..)
+!             type(MPI_Op),      intent(in)                :: op
+!             type(MPI_Request)                            :: request
+! !             call MPI_Ireduce(sendbuf, recvbuf, size(recvbuf), MPI_INT, &
+! !                              op, 0, comm, request, mpi_err)
+!         end subroutine mpi_integer_ireduce
+!
+!         subroutine mpi_double_reduce(sendbuf, recvbuf, op)
+!             double precision, intent(in)  :: sendbuf(..)
+!             double precision, intent(out) :: recvbuf(..)
+!             type(MPI_Op),     intent(in)  :: op
+!             call MPI_Reduce(sendbuf, recvbuf, size(recvbuf), MPI_DOUBLE, &
+!                             op, 0, comm, mpi_err)
+!         end subroutine mpi_double_reduce
+!
+!         subroutine mpi_integer_reduce(sendbuf, recvbuf, op)
+!             integer,      intent(in)  :: sendbuf(..)
+!             integer,      intent(out) :: recvbuf(..)
+!             type(MPI_Op), intent(in)  :: op
+!             call MPI_Reduce(sendbuf, recvbuf, size(recvbuf), MPI_INT, &
+!                             op, 0, comm, mpi_err)
+!         end subroutine mpi_integer_reduce
 
 end module mpi_collectives
--- src/mpi/mpi_p2p.f90
+++ src/mpi/mpi_p2p.f90
@@ -2,25 +2,25 @@ module mpi_p2p
     use mpi_communicator
     implicit none
 
-    interface mpi_non_blocking_send
-        module procedure :: mpi_double_isend
-        module procedure :: mpi_integer_isend
-    end interface mpi_non_blocking_send
-
-    interface mpi_non_blocking_recv
-        module procedure :: mpi_double_irecv
-        module procedure :: mpi_integer_irecv
-    end interface mpi_non_blocking_recv
-
-    interface mpi_blocking_send
-        module procedure :: mpi_double_send
-        module procedure :: mpi_integer_send
-    end interface mpi_blocking_send
-
-    interface mpi_blocking_recv
-        module procedure :: mpi_double_recv
-        module procedure :: mpi_integer_recv
-    end interface mpi_blocking_recv
+!     interface mpi_non_blocking_send
+!         module procedure :: mpi_double_isend
+!         module procedure :: mpi_integer_isend
+!     end interface mpi_non_blocking_send
+!
+!     interface mpi_non_blocking_recv
+!         module procedure :: mpi_double_irecv
+!         module procedure :: mpi_integer_irecv
+!     end interface mpi_non_blocking_recv
+!
+!     interface mpi_blocking_send
+!         module procedure :: mpi_double_send
+!         module procedure :: mpi_integer_send
+!     end interface mpi_blocking_send
+!
+!     interface mpi_blocking_recv
+!         module procedure :: mpi_double_recv
+!         module procedure :: mpi_integer_recv
+!     end interface mpi_blocking_recv
 
 
     contains
@@ -30,72 +30,72 @@ module mpi_p2p
         ! Uses 'asynchronous', see e.g. https://www.open-mpi.org/doc/current/man3/MPI_Isend.3.php
         ! and https://stackoverflow.com/questions/19455051/using-mpi-send-recv-to-handle-chunk-of-multi-dim-array-in-fortran-90 (23 November 2021)
         !
-        subroutine mpi_double_isend(data, dest, tag)
-            double precision, intent(in), asynchronous :: data(..)
-            integer,          intent(in)               :: dest
-            integer,          intent(in)               :: tag
-            type(MPI_Request)                          :: request
-            call MPI_Isend(data, size(data), MPI_DOUBLE, dest, tag, comm, request, mpi_err)
-        end subroutine mpi_double_isend
-
-        subroutine mpi_integer_isend(data, dest, tag)
-            integer, intent(in), asynchronous :: data(..)
-            integer, intent(in)               :: dest
-            integer, intent(in)               :: tag
-            type(MPI_Request)                 :: request
-            call MPI_Isend(data, size(data), MPI_INT, dest, tag, comm, request, mpi_err)
-        end subroutine mpi_integer_isend
-
-
-        subroutine mpi_double_irecv(data, source, tag)
-            double precision, intent(out), asynchronous :: data(..)
-            integer,          intent(in)                :: source
-            integer,          intent(in)                :: tag
-            type(MPI_Request)                           :: request
-            call MPI_Irecv(data, size(data), MPI_DOUBLE, source, tag, comm, request, mpi_err)
-        end subroutine mpi_double_irecv
-
-        subroutine mpi_integer_irecv(data, source, tag)
-            integer, intent(out), asynchronous :: data(..)
-            integer, intent(in)                :: source
-            integer, intent(in)                :: tag
-            type(MPI_Request)                  :: request
-            call MPI_Irecv(data, size(data), MPI_INT, source, tag, comm, request, mpi_err)
-        end subroutine mpi_integer_irecv
+!         subroutine mpi_double_isend(data, dest, tag)
+!             double precision, intent(in), asynchronous :: data(..)
+!             integer,          intent(in)               :: dest
+!             integer,          intent(in)               :: tag
+!             type(MPI_Request)                          :: request
+!             call MPI_Isend(data, size(data), MPI_DOUBLE, dest, tag, comm, request, mpi_err)
+!         end subroutine mpi_double_isend
+!
+!         subroutine mpi_integer_isend(data, dest, tag)
+!             integer, intent(in), asynchronous :: data(..)
+!             integer, intent(in)               :: dest
+!             integer, intent(in)               :: tag
+!             type(MPI_Request)                 :: request
+!             call MPI_Isend(data, size(data), MPI_INT, dest, tag, comm, request, mpi_err)
+!         end subroutine mpi_integer_isend
+!
+!
+!         subroutine mpi_double_irecv(data, source, tag)
+!             double precision, intent(out), asynchronous :: data(..)
+!             integer,          intent(in)                :: source
+!             integer,          intent(in)                :: tag
+!             type(MPI_Request)                           :: request
+!             call MPI_Irecv(data, size(data), MPI_DOUBLE, source, tag, comm, request, mpi_err)
+!         end subroutine mpi_double_irecv
+!
+!         subroutine mpi_integer_irecv(data, source, tag)
+!             integer, intent(out), asynchronous :: data(..)
+!             integer, intent(in)                :: source
+!             integer, intent(in)                :: tag
+!             type(MPI_Request)                  :: request
+!             call MPI_Irecv(data, size(data), MPI_INT, source, tag, comm, request, mpi_err)
+!         end subroutine mpi_integer_irecv
 
 
         !
         ! blocking send/receive
         !
-        subroutine mpi_double_send(data, dest, tag)
-            double precision, intent(in) :: data(..)
-            integer,          intent(in) :: dest
-            integer,          intent(in) :: tag
-            call MPI_Send(data, size(data), MPI_DOUBLE, dest, tag, comm, mpi_err)
-        end subroutine mpi_double_send
-
-        subroutine mpi_integer_send(data, dest, tag)
-            integer, intent(in) :: data(..)
-            integer, intent(in) :: dest
-            integer, intent(in) :: tag
-            call MPI_Send(data, size(data), MPI_INT, dest, tag, comm, mpi_err)
-        end subroutine mpi_integer_send
-
-
-        subroutine mpi_double_recv(data, source, tag)
-            double precision, intent(out) :: data(..)
-            integer,          intent(in)  :: source
-            integer,          intent(in)  :: tag
-            type(MPI_Status)              :: status
-            call MPI_Recv(data, size(data), MPI_DOUBLE, source, tag, comm, status, mpi_err)
-        end subroutine mpi_double_recv
-
-        subroutine mpi_integer_recv(data, source, tag)
-            integer, intent(out) :: data(..)
-            integer, intent(in)  :: source
-            integer, intent(in)  :: tag
-            type(MPI_Status)     :: status
-            call MPI_Recv(data, size(data), MPI_INT, source, tag, comm, status, mpi_err)
-        end subroutine mpi_integer_recv
+!         subroutine mpi_double_send(data, dest, tag)
+!             double precision, intent(in) :: data(..)
+!             integer,          intent(in) :: dest
+!             integer,          intent(in) :: tag
+!             call MPI_Send(data, size(data), MPI_DOUBLE, dest, tag, comm, mpi_err)
+!         end subroutine mpi_double_send
+!
+!         subroutine mpi_integer_send(data, dest, tag)
+!             integer, intent(in) :: data(..)
+!             integer, intent(in) :: dest
+!             integer, intent(in) :: tag
+!             call MPI_Send(data, size(data), MPI_INT, dest, tag, comm, mpi_err)
+!         end subroutine mpi_integer_send
+!
+!
+!         subroutine mpi_double_recv(data, source, tag)
+!             double precision, intent(out) :: data(..)
+!             integer,          intent(in)  :: source
+!             integer,          intent(in)  :: tag
+!             type(MPI_Status)              :: status
+!             call MPI_Recv(data, size(data), MPI_DOUBLE, source, tag, comm, status, mpi_err)
+!         end subroutine mpi_double_recv
+!
+!         subroutine mpi_integer_recv(data, source, tag)
+!             integer, intent(out) :: data(..)
+!             integer, intent(in)  :: source
+!             integer, intent(in)  :: tag
+!             type(MPI_Status)     :: status
+!             call MPI_Recv(data, size(data), MPI_INT, source, tag, comm, status, mpi_err)
+!         end subroutine mpi_integer_recv
 
 end module mpi_p2p
