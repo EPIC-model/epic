e2608aa081a48dc42c61efbcae28bae4a553c79b
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-14 14:30:32 +0100

    add mpi_print and mpi_exit_on_error

--- src/mpi/mpi_utils.f90
+++ src/mpi/mpi_utils.f90
@@ -3,5 +3,17 @@ module mpi_utils
     implicit none
 
     contains
-        ! TODO
+        subroutine mpi_print(msg)
+            character(*), intent(in) :: msg
+            if (mpi_rank == 0) then
+                print *, msg
+            endif
+        end subroutine mpi_print
+
+        subroutine mpi_exit_on_error(msg)
+            character(*), intent(in) :: msg
+            print *, "Error on rank ", mpi_rank
+            print *, "    ", msg
+            call MPI_Abort(comm, -1, mpi_err)
+        end subroutine mpi_exit_on_error
 end module mpi_utils
