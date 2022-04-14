cf7c3395eb08eeeb442779112c9172e708f9913a
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-14 08:39:12 +0100

    mpi init unit test

--- unit-tests/mpi/test_mpi_init.f90
+++ unit-tests/mpi/test_mpi_init.f90
@@ -10,18 +10,22 @@ program test_mpi_init
 
     logical :: passed = .false.
 
-    print *, "HI"
-
     call mpi_comm_initialise
 
-    print *, "HI"
+    if (mpi_size == 1) then
+        print *, "MPI tests must be run with more than one process."
+        call mpi_comm_finalise
+        stop
+    endif
 
     passed = (mpi_err == 0)
 
-!     call mpi_comm_finalise
+    call mpi_comm_finalise
 
-!     passed = (passed .and. (mpi_err .ne. 0))
+    passed = (passed .and. (mpi_err == 0))
 
-    call print_result_logical('Test MPI init', passed)
+    if (mpi_rank == 0) then
+        call print_result_logical('Test MPI init', passed)
+    endif
 
 end program test_mpi_init
