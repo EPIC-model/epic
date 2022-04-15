4fbab951bc811ccaa9b6fb418f48385b8173a2d8
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-15 09:28:46 +0100

    add structure for parcel exchange

--- src/3d/parcels/parcel_mpi.f90
+++ src/3d/parcels/parcel_mpi.f90
@@ -1,4 +1,52 @@
 module parcel_mpi
+    use fields, only : get_index
+    use field_layout
     implicit none
+
+    private :: locate_parcels,  &
+               pack_parcels,    &
+               unpack_parcels
+
+    contains
+
+        subroutine parcel_exchange
+            integer :: pid(n_parcels)
+
+            ! figure out where parcels should go
+            call parcel_locate(pid)
+
+            call pack_parcels
+
+            ! send parcels
+
+            ! receive parcels
+
+            call unpack_parcels
+
+        end subroutine parcel_exchange
+
+        subroutine parcel_locate(pid)
+            integer, intent(out) :: pid(:)
+            integer              :: i, j, k, n
+
+            do n = 1, n_parcels
+                call get_index(parcels%position(:, n), i, j, k)
+
+!                 if (is_outside(i, j)) then
+
+!                 endif
+
+            enddo
+
+        end subroutine parcel_locate
+
+        subroutine pack_parcels
+
+        end subroutine pack_parcels
+
+
+        subroutine unpack_parcels
+
+        end subroutine unpack_parcels
 
 end module parcel_mpi
