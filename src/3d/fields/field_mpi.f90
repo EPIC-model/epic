9025ca69e780a3819ddb2e80c63c305920be5aeb
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-20 12:07:27 +0100

    default structure

--- src/3d/fields/field_mpi.f90
+++ src/3d/fields/field_mpi.f90
@@ -2,4 +2,36 @@ module field_mpi
     use mpi_layout
     implicit none
 
+    contains
+
+        subroutine field_halo_fill(data)
+            double precision :: data(box%hlo(3):box%hhi(3), &
+                                     box%hlo(2):box%hhi(2), &
+                                     box%hlo(1):box%hhi(1))
+
+            ! send to left process
+            call MPI_Send(data(box%lo(3)
+
+            ! receive from right process
+
+
+            ! send to right process
+
+            ! receive from left process
+
+
+            ! send to lower process
+
+            ! receive from upper process
+
+            ! send to upper process
+
+            ! receive from lower process
+
+        end subroutine field_halo_fill
+
+        subroutine field_halo_accumulate(data)
+
+        end subroutine field_halo_accumulate
+
 end module field_mpi
