47708fec0f5cce8f1684980ea4668e630dd79577
Matthias Frey <mf248@st-andrews.ac.uk>
2022-05-01 18:25:25 +0100

    parallel par2grid works

--- unit-tests/3d/test_mpi_trilinear.f90
+++ unit-tests/3d/test_mpi_trilinear.f90
@@ -13,14 +13,18 @@ program test_mpi_trilinear
     use parcel_ellipsoid, only : get_abc
     use parameters, only : lower, update_parameters, vcell, dx, nx, ny, nz, ngrid
     use fields, only : volg, field_alloc
+    use field_ops, only : get_sum
     use timer
     implicit none
 
     double precision :: error
     integer          :: ix, iy, iz, i, j, k, l, n_per_dim
     double precision :: im, corner(3)
+    logical          :: passed = .true.
 
     call mpi_comm_initialise
+
+    passed = (passed .and. (mpi_err == 0))
 
     nx = 32
     ny = 32
@@ -63,20 +67,26 @@ program test_mpi_trilinear
 
     parcels%volume = vcell / dble(n_per_dim ** 3)
 
-    parcels%B(:, :) = zero
+    parcels%B(:, 1:n_parcels) = zero
 
     ! b11
-    parcels%B(1, :) = get_abc(parcels%volume(1:n_parcels)) ** f23
+    parcels%B(1, 1:n_parcels) = get_abc(parcels%volume(1:n_parcels)) ** f23
 
     ! b22
-    parcels%B(4, :) = parcels%B(1, :)
+    parcels%B(4, 1:n_parcels) = parcels%B(1, 1:n_parcels)
 
     call par2grid
 
-    error = abs(sum(volg(0:nz, :, :)) - dble(ngrid) * vcell)
+    error = abs(get_sum(volg)) - dble(ngrid) * vcell
+
+    passed = (passed .and. (error < dble(3.0e-14)))
 
     call mpi_comm_finalise
 
-    call print_result_dp('Test MPI trilinear (par2grid)', error, atol=dble(3.0e-14))
+    passed = (passed .and. (mpi_err == 0))
+
+    if (mpi_rank == mpi_master) then
+        call print_result_logical('Test MPI trilinear (par2grid)', passed)
+    endif
 
 end program test_mpi_trilinear
