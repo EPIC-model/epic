dd570c8b82dbbeae009019ed77d9fda45c972264
Matthias Frey <mf248@st-andrews.ac.uk>
2022-05-04 14:49:40 +0100

    load subroutine

--- src/3d/epic3d.f90
+++ src/3d/epic3d.f90
@@ -14,7 +14,8 @@ program epic3d
                                   apply_vortcor,          &
                                   lapl_corr_timer,        &
                                   grad_corr_timer,        &
-                                  vort_corr_timer
+                                  vort_corr_timer,        &
+                                  init_parcel_correction
     use parcel_diagnostics, only : init_parcel_diagnostics, &
                                    parcel_stats_timer
     use parcel_netcdf, only : parcel_io_timer, read_netcdf_parcels
