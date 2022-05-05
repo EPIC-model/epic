6d550796f197e1635f18e82b0ad16f63a4e76a75
Matthias Frey <mf248@st-andrews.ac.uk>
2022-05-05 19:14:01 +0100

    do vorticity tendency correction

--- src/3d/epic3d.f90
+++ src/3d/epic3d.f90
@@ -149,7 +149,7 @@ program epic3d
 #endif
                 call ls_rk4_step(t)
 
-                call apply_vortcor
+!                 call apply_vortcor
 
                 call merge_parcels(parcels)
 
