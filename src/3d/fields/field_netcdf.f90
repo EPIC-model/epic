fb3c9208c598440df683c93ec355a8bec2b48d7a
Matthias Frey <mf248@st-andrews.ac.uk>
2022-09-28 10:13:18 +0100

    clean up

--- src/3d/fields/field_netcdf.f90
+++ src/3d/fields/field_netcdf.f90
@@ -308,49 +308,6 @@ module field_netcdf
         subroutine write_netcdf_fields(t)
             double precision, intent(in) :: t
             integer                      :: cnt(4), start(4)
-!            integer :: ix, iy, iz
-!            double precision :: alpha, fk2l2, k, m, l, x, y, z
-!            double precision :: vortg_ref(-1:nz+1, 0:ny-1, 0:nx-1, 3)
-!            double precision :: velog_ref(-1:nz+1, 0:ny-1, 0:nx-1, 3)
-!            double precision :: vtend_ref(-1:nz+1, 0:ny-1, 0:nx-1, 3)
-!            double precision :: cosmz, sinmz, sinkxly, coskxly
-!
-!            k = two
-!            l = two
-!            m = one
-!
-!            alpha = dsqrt(k ** 2 + l ** 2 + m ** 2)
-!            fk2l2 = one / dble(k ** 2 + l ** 2)
-!
-!            do ix = 0, nx-1
-!               x = lower(1) + ix * dx(1)
-!               do iy = 0, ny-1
-!                  y = lower(2) + iy * dx(2)
-!                  do iz = -1, nz+1
-!                     z = lower(3) + iz * dx(3)
-!
-!                     cosmz = dcos(m * z)
-!                     sinmz = dsin(m * z)
-!                     sinkxly = dsin(k * x + l * y)
-!                     coskxly = dcos(k * x + l * y)
-!
-!                     ! velocity
-!                     velog_ref(iz, iy, ix, 1) = fk2l2 * (k * m * sinmz - l * alpha * cosmz) * sinkxly
-!                     velog_ref(iz, iy, ix, 2) = fk2l2 * (l * m * sinmz + k * alpha * cosmz) * sinkxly
-!                     velog_ref(iz, iy, ix, 3) = cosmz * coskxly
-!
-!                     ! vorticity
-!                     vortg_ref(iz, iy, ix, 1) = alpha * velog_ref(iz, iy, ix, 1)
-!                     vortg_ref(iz, iy, ix, 2) = alpha * velog_ref(iz, iy, ix, 2)
-!                     vortg_ref(iz, iy, ix, 3) = alpha * velog_ref(iz, iy, ix, 3)
-!
-!                     ! reference solution
-!                     vtend_ref(iz, iy, ix, 1) = alpha * k * m ** 2 * fk2l2 * sinkxly * coskxly
-!                     vtend_ref(iz, iy, ix, 2) = alpha * l * m ** 2 * fk2l2 * sinkxly * coskxly
-!                     vtend_ref(iz, iy, ix, 3) = -alpha * m * sinmz * cosmz
-!                  enddo
-!               enddo
-!            enddo
 
             call start_timer(field_io_timer)
 
@@ -375,9 +332,6 @@ module field_netcdf
             !
             ! write fields (do not write halo cells)
             !
-!            velog = velog - velog_ref
-!            vortg = vortg - vortg_ref
-!            vtend = vtend - vtend_ref
 
             call write_netcdf_dataset(ncid, x_vel_id, velog(0:nz, 0:ny-1, 0:nx-1, 1), &
                                       start, cnt)
