72ac0c0e2f8415ef23debde4871506a6ef254743
Matthias Frey <mf248@st-andrews.ac.uk>
2023-01-09 10:55:54 +0000

    use package bm

--- python-scripts/tools/mpl_style.py
+++ python-scripts/tools/mpl_style.py
@@ -19,6 +19,7 @@ mpl.rcParams.update({
         r"\usepackage{amsmath}",
         r"\usepackage[utf8]{inputenc}",
         r"\usepackage[T1]{fontenc}",
+        r"\usepackage{bm}",
         r"\usepackage{siunitx}",
         ])
     #"pgf.rcfonts": False,
