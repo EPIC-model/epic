930ba2db6501972ca5303a8e09ec940bd51fe409
Matthias Frey <mf248@st-andrews.ac.uk>
2023-05-26 11:17:58 +0100

    unit tests for new eigenvalue solver

--- src/utils/scherzinger.f90
+++ src/utils/scherzinger.f90
@@ -8,13 +8,14 @@
 !       ISSN 0045-7825, https://doi.org/10.1016/j.cma.2008.03.031.
 ! =============================================================================
 module scherzinger
-    use constants, only : one, zero, f12, f13, f32, fpi6, three, pi, two, four
+    use constants, only : one, zero, f12, f13, f32, fpi6, three, pi, two, four, f23
     use linalg, only : cross, determinant, trace, signum
     implicit none
 
     private
         double precision, parameter :: atol = 1.0e-12
         double precision            :: r(3, 3)
+        double precision            :: eta(3)
 
     public :: scherzinger_diagonalise, scherzinger_eigenvalues
 
@@ -55,26 +56,28 @@ module scherzinger
                 return
             endif
 
-            call scherzinger_eigenvalues(A, D)
+            call scherzinger_eigenvalues(A, D, .false.)
 
+            A(1, 1) = A(1, 1) - eta(2)
+            A(2, 2) = A(2, 2) - eta(2)
+            A(3, 3) = A(3, 3) - eta(2)
+
+            ! first eigenvector, eq 21:
             V(:, 1) = cross(r(:, 1), r(:, 2))
 
-            ! u1 and u2
+            ! u1 and u2, eq 30:
             rlen = matmul(A, r(:, 1))
             r(:, 1) = rlen
             rlen = matmul(A, r(:, 2))
             r(:, 2) = rlen
 
             rlen = norm2(r, dim=1)
-
             i = maxloc(rlen(1:2), dim=1)
-
             V(:, 3) = r(:, i) / rlen(i)
 
-
+            ! second and third eigenvector, eq 31:
             V(:, 2) = cross(V(:, 3), V(:, 1))
             V(:, 3) = cross(V(:, 1), V(:, 2))
-
 
             call sort_descending(D, V)
 
@@ -121,45 +124,42 @@ module scherzinger
         ! where D is a diagonal matrix with the eigenvalues and
         ! V is the eigenvector matrix. The eigenvalues are in
         ! descending order.
-        ! The input matrix is overwritten, the diagonal
-        ! entries will be storing the eigenvalues
+        ! The input matrix is overwritten.
         ! @param[inout] A real symmetric 3x3 matrix
         ! @param[out] D eigenvalues in descending order
-        subroutine scherzinger_eigenvalues(A, D)
+        subroutine scherzinger_eigenvalues(A, D, l_sort)
             double precision, intent(inout) :: A(3, 3)
             double precision, intent(out)   :: D(3)
+            logical,          intent(in)    :: l_sort
             double precision                :: AA(3, 3)
-            double precision                :: tr, j2, j3, alpha, eta(3)
+            double precision                :: tr, j2, j3, alpha
             double precision                :: tmp, smp
             double precision                :: s1As1, s1As2, s2As1, s2As2, rlen(3)
             integer                         :: i
 
-            ! store initial diagonal values
-            D(1) = A(1, 1)
-            D(2) = A(2, 2)
-            D(3) = A(3, 3)
-
-            ! deviatoric matrix:
+            ! deviatoric matrix (eq 2):
             tr = trace(A)
             tmp = f13 * tr
             A(1, 1) = A(1, 1) - tmp
             A(2, 2) = A(2, 2) - tmp
             A(3, 3) = A(3, 3) - tmp
 
-            ! invariants j2 and j3:
+            ! invariants j2 and j3 (eq 5):
             AA = matmul(A, A)
             j2 = f12 * trace(AA)
             j3 = determinant(A)
 
-            ! angle:
+            ! angle (solve eq 7 for alpha):
             alpha = f13 * dacos(f12 * j3 * (three / j2) ** f32)
 
             if (alpha > fpi6) then
-                alpha = alpha + two * f13 * pi
+                alpha = alpha + f23 * pi
             endif
 
+            ! eq 6:
             eta(1) = two * dsqrt(f13 * j2) * dcos(alpha)
 
+            ! eq 19:
             r(:, 1) = (/ A(1, 1) - eta(1), A(2, 1),          A(3, 1)          /)
             r(:, 2) = (/ A(1, 2),          A(2, 2) - eta(1), A(3, 2)          /)
             r(:, 3) = (/ A(1, 3),          A(2, 3),          A(3, 3) - eta(1) /)
@@ -170,21 +170,18 @@ module scherzinger
             i = maxloc(rlen, dim=1)
 
             ! s1
-            if (i == 1) then
-                r(:, 1) = r(:, i) / rlen(i)
-            else if (i == 2) then
-                rlen = r(:, 2) / rlen(2)
+            rlen = r(:, i) / rlen(i)
 
+            if (i == 2) then
                 r(:, 2) = r(:, 1)
-                r(:, 1) = rlen
-            else
-                ! i == 3
-                rlen = r(:, 3) / rlen(3)
-
+            else if (i == 3) then
                 r(:, 3) = r(:, 1)
-                r(:, 1) = rlen
             endif
 
+            ! s1
+            r(:, 1) = rlen
+
+            ! t2 and t3 of eq 20:
             tmp = dot_product(r(:, 1), r(:, 2))
             smp = dot_product(r(:, 1), r(:, 3))
             r(:, 2) = r(:, 2) - tmp * r(:, 1)
@@ -193,38 +190,38 @@ module scherzinger
             ! length of each r vector
             rlen = norm2(r, dim=1)
 
-            i = maxloc(rlen(2:3), dim=1)
-
             ! s2
+            i = maxloc(rlen(2:3), dim=1)
             r(:, 2) = r(:, i+1) / rlen(i+1)
 
+            ! first eigenvector, eq 21:
 !             V(:, 1) = cross(r(:, 1), r(:, 2))
 
+            ! eq 22:
             rlen = matmul(A, r(:, 1))
             s1As1 = dot_product(r(:, 1), rlen)
             s2As1 = dot_product(r(:, 2), rlen)
 
             rlen = matmul(A, r(:, 2))
-            s1As2 = dot_product(r(:, 1), rlen)
+            s1As2 = s2As1 !dot_product(r(:, 1), rlen)
             s2As2 = dot_product(r(:, 2), rlen)
 
             tmp = s1As1 - s2As2
             smp = s1As1 + s2As2
 
+            ! second eigenvalue, eq 27:
             eta(2) = f12 * smp                                                    &
-                   - f12 * signum(tmp) * sqrt(tmp ** 2 + four * s1As2 * s2As1)
+                   - f12 * signum(tmp) * dsqrt(tmp ** 2 + four * s1As2 * s2As1)
 
+            !  third eigenvalue, eq 27:
             eta(3) = smp - eta(2)
 
-            A(1, 1) = D(1) - eta(2)
-            A(2, 2) = D(2) - eta(2)
-            A(3, 3) = D(3) - eta(2)
-
-
-
+            ! final eigenvalues:
             D = eta + f13 * tr
 
-            call sort_eigenvalues(D)
+            if (l_sort) then
+                call sort_eigenvalues(D)
+            endif
 
         end subroutine scherzinger_eigenvalues
 
