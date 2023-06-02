f625e8d1a3dca37ae48ba84182f20794dc105867
Matthias Frey <mf248@st-andrews.ac.uk>
2023-06-02 14:48:06 +0100

    fix typo

--- src/3d/boundary_layer/bndry_fluxes.f90
+++ src/3d/boundary_layer/bndry_fluxes.f90
@@ -199,7 +199,7 @@ module bndry_fluxes
             ! local maximum of absolute value (units: m/s**3)
             abs_max = maxval(dabs(binc(box%lo(2):box%hi(2), box%lo(1):box%hi(1))))
 
-            ! get gloval abs_max
+            ! get global abs_max
             call MPI_Allreduce(MPI_IN_PLACE,            &
                                abs_max,                 &
                                1,                       &
