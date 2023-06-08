9e7dab44705c6003a14c72d298690c331b106f66
Matthias Frey <mf248@st-andrews.ac.uk>
2023-06-08 11:37:02 +0100

    old_size --> n_copy

--- src/utils/armanip.f90
+++ src/utils/armanip.f90
@@ -18,16 +18,20 @@ module armanip
 
         !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-        subroutine resize_array_1d(attr, new_size, old_size)
+        ! Resize a one-dimensional array
+        ! @param[inout] attr the array
+        ! @param[in] new_size of the array
+        ! @param[in] n_copy number of elements to copy over
+        subroutine resize_array_1d(attr, new_size, n_copy)
             double precision, allocatable, intent(inout) :: attr(:)
             integer,                       intent(in)    :: new_size
-            integer, optional,             intent(in)    :: old_size
+            integer, optional,             intent(in)    :: n_copy
             double precision, allocatable                :: buffer(:)
             integer                                      :: copy_size
 
             copy_size = size(attr)
-            if (present(old_size)) then
-                copy_size = old_size
+            if (present(n_copy)) then
+                copy_size = n_copy
             endif
 
             if (new_size < copy_size) then
@@ -44,18 +48,22 @@ module armanip
 
         !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-        subroutine resize_array_2d(attr, new_size, old_size)
+        ! Resize a two-dimensional array
+        ! @param[inout] attr the array
+        ! @param[in] new_size of the array
+        ! @param[in] n_copy number of elements to copy over
+        subroutine resize_array_2d(attr, new_size, n_copy)
             double precision, allocatable, intent(inout) :: attr(:, :)
             integer,                       intent(in)    :: new_size
-            integer, optional,             intent(in)    :: old_size
+            integer, optional,             intent(in)    :: n_copy
             double precision, allocatable                :: buffer(:, :)
             integer                                      :: ncomp, copy_size
 
             ncomp = size(attr, dim=1)
             copy_size = size(attr, dim=2)
 
-            if (present(old_size)) then
-                copy_size = old_size
+            if (present(n_copy)) then
+                copy_size = n_copy
             endif
 
             if (new_size < copy_size) then
