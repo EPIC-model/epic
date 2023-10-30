3b2796314542008809c494370d5aa38bf1f29d6a
Rui Apóstolo <ruiapostolo@gmail.com>
2023-10-30 09:08:30 +0000

    Changes to parcel_split

--- src/3d/parcels/parcel_split.f90
+++ src/3d/parcels/parcel_split.f90
@@ -39,7 +39,7 @@ module parcel_split_mod
         ! parcels with aspect ratios larger than parcel%lambda_max.
         subroutine parcel_split
             double precision     :: B(6)
-            double precision     :: vol, lam
+            double precision     :: lam
             double precision     :: D(3), V(3, 3)
             integer              :: last_index, n_indices
             integer              :: grown_size, shrunk_size, n_required
@@ -56,12 +56,12 @@ module parcel_split_mod
             !------------------------------------------------------------------
             ! Check which parcels split and store the indices in *pid*:
             !$omp parallel default(shared)
-            !$omp do private(n, B, vol, lam, D)
+            !$omp do private(n, B, lam, D)
             do n = 1, n_parcels
                 B = parcels%B(:, n)
-                vol = parcels%volume(n)
+                ! vol = parcels%volume(n)
 
-                D = get_eigenvalues(B, vol)
+                D = get_eigenvalues(B)
 
                 ! evaluate maximum aspect ratio (a2 >= b2 >= c2)
                 lam = get_aspect_ratio(D)
