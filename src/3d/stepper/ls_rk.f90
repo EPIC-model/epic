ce267b160dcf1544bc2eb1c7eca3aee8e1bb1421
Matthias Frey <mf248@st-andrews.ac.uk>
2023-11-13 15:01:11 +0000

    check time step every time

--- src/3d/stepper/ls_rk.f90
+++ src/3d/stepper/ls_rk.f90
@@ -100,9 +100,9 @@ module ls_rk
             ! update the time step
             dt = get_time_step(t)
 
-            if (dabs(t - time%initial) < 1.0e-13) then
+!            if (dabs(t - time%initial) < 1.0e-13) then
                 call bndry_fluxes_time_step(dt)
-            endif
+!            endif
 
             call grid2par
 
