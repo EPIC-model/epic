c49a36454dac9188505a4ea18b91ba37f42adb6b
sjboeing <sjboing@gmail.com>
2024-10-09 14:03:59 +0100

    Ensure parcel periodic bc works for arbitrary input

--- src/3d/parcels/parcel_bc.f90
+++ src/3d/parcels/parcel_bc.f90
@@ -5,7 +5,7 @@
 ! =============================================================================
 module parcel_bc
     use constants, only : zero, two
-    use parameters, only : lower, upper, extent, hli, center
+    use parameters, only : lower, upper, extent, extenti
     use parcel_container, only : n_parcels, parcels
     use omp_lib
     implicit none
@@ -18,15 +18,15 @@ module parcel_bc
             double precision, intent(inout) :: position(3)
 
             if(position(1) >= upper(1)) then
-                position(1) = position(1) - extent(1)
+                position(1) = position(1) - int(1+(position(1)-upper(1))*extenti(1))*extent(1)
             else if (position(1) < lower(1)) then
-                position(1) = position(1) + extent(1) 
+                position(1) = position(1) + int(1+(lower(1)-position(1))*extenti(1))*extent(1)
             endif
             
             if(position(2) >= upper(2)) then
-                position(2) = position(2) - extent(2) 
+                position(2) = position(2) - int(1+(position(2)-upper(2))*extenti(2))*extent(2)
             else if (position(2) < lower(2)) then
-                position(2) = position(2) + extent(2) 
+                position(2) = position(2) + int(1+(lower(2)-position(2))*extenti(2))*extent(2)
             endif
 
         end subroutine apply_periodic_bc
